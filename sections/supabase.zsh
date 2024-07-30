b738543642f41d937c06b42ba4a7c804f9abb02a
Aaron Merryman <amerryma@asu.edu>
2024-07-30 09:12:52 -0700

    feat: better caching for when we switch directories

--- sections/supabase.zsh
+++ sections/supabase.zsh
@@ -1,7 +1,7 @@
 #
 # Supabase
 #
-# Supabase is a supa-dupa cool tool for making you development easier.
+# Supabase is a supa-dupa cool tool for making your development easier.
 # Link: https://www.supabase.io
 
 # ------------------------------------------------------------------------------
@@ -19,15 +19,28 @@ SPACESHIP_SUPABASE_COLOR="${SPACESHIP_SUPABASE_COLOR="#85E0B7"}"
 # Section
 # ------------------------------------------------------------------------------
 
-# Define the cache file location and the cache duration (in seconds)
-CACHE_FILE="$HOME/.cache/supabase_projects_cache"
+# Define the cache directory and duration (in seconds)
+CACHE_DIR="$HOME/.cache/supabase_projects_cache"
 CACHE_DURATION=600  # 10 minutes
+
+# Ensure the cache directory exists
+mkdir -p "$CACHE_DIR"
+
+# Safely delete all cache files in the directory
+find "$CACHE_DIR" -type f -name '*_cache' -delete
+
+# Function to get the directory-specific cache file path
+get_cache_file() {
+  echo "$CACHE_DIR/$(echo "$PWD" | md5sum | cut -d ' ' -f 1)_cache"
+}
 
 # Function to check if the cache is still valid
 is_cache_valid() {
-  if [[ -f "$CACHE_FILE" ]]; then
+  local cache_file
+  cache_file=$(get_cache_file)
+  if [[ -f "$cache_file" ]]; then
     local cache_mtime
-    cache_mtime=$(stat -c %Y "$CACHE_FILE")
+    cache_mtime=$(stat -c %Y "$cache_file")
     local current_time
     current_time=$(date +%s)
     local age=$((current_time - cache_mtime))
@@ -39,21 +52,28 @@ is_cache_valid() {
 
 # Function to update the cache
 update_cache() {
-  supabase projects list > "$CACHE_FILE"
+  local cache_file
+  cache_file=$(get_cache_file)
+  supabase projects list | awk -F '│' '$1 ~ /●/ { gsub(/^ +| +$/, "", $4); print $4 }' > "$cache_file"
 }
 
 spaceship_supabase() {
   [[ $SPACESHIP_SUPABASE_SHOW == false ]] && return
   spaceship::exists supabase || return
-  [ ! -f supabase/.temp/project-ref ] && return
 
+  local cache_file
+  cache_file=$(get_cache_file)
   local supabase_project_name
 
   if ! is_cache_valid; then
-    echo "Updating cache"
     update_cache
   fi
-  supabase_project_name="$(awk -F '│' '$1 ~ /●/ { gsub(/^ +| +$/, "", $4); print $4 }' "$CACHE_FILE")"
+
+  supabase_project_name="$(cat "$cache_file")"
+
+  if [[ -z "$supabase_project_name" ]]; then
+    return
+  fi
 
   spaceship::section::v4 \
     --color "$SPACESHIP_SUPABASE_COLOR" \
